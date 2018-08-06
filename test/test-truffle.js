47c915b24b0b77c2dff1951c761ad62afd8d87f5
rocky <rb@dustyfeet.com>
2018-08-06 18:28:10 -0400

    Get more home-grown trufstuf where we can start to do things.

--- test/test-truffle.js
+++ test/test-truffle.js
@@ -1,7 +1,18 @@
-trufstuf = require("../lib/trufstuf.js")
-assert = require('assert');
+"use strict";
+
+var trufstuf = require("../lib/trufstuf.js")
+var assert = require('assert');
 
 assert(trufstuf.is_truffle_root('.') == false);
 assert(trufstuf.is_truffle_root('./sample-truffle') == true);
+var build_dir = trufstuf.get_build_contracts_dir('./sample-truffle')
 assert(trufstuf.get_build_contracts_dir('./sample-truffle')
        == './sample-truffle/build/contracts');
+var json_files = trufstuf.get_truffle_build_json_files(build_dir);
+var expected = [ 'integeroverflow.json', 'migrations.json', 'mythril.json'];
+assert(expected.length == json_files.length &&
+       expected.every(function(item, i) {
+	   return item === expected[i];
+       }));
+assert (trufstuf.guess_truffle_build_json(build_dir)
+	== 'IntegerOverflow.json')
