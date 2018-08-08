1cf01b716aba58b73d5c330f06ae3d7e542a770e
rocky <rb@dustyfeet.com>
2018-08-08 03:25:59 -0400

    Minimal dynamic array detection via sourcemap

---
+++ test/test-srcmap.js
@@ -0,0 +1,25 @@
+"use strict"
+
+var assert = require('assert');
+var SourceMappingDecoder = require('remix-lib/src/sourceMappingDecoder.js')
+var compilerInput = require('remix-lib/src/helpers/compilerHelper').compilerInput
+
+var srcmap = require("../lib/srcmap.js")
+var contract = `pragma solidity ^0.4.22;
+
+contract PublicStorageArray {
+    bytes32[] public STATES = [bytes32(0)];
+}
+`
+
+var output = srcmap.compileContract(contract)
+
+assert(output)
+
+var sourceMap = output.contracts['test.sol']['PublicStorageArray']
+    .evm.deployedBytecode.sourceMap;
+var node = srcmap.isVariableDeclaration(151, sourceMap, output.sources['test.sol'])
+
+assert(node)
+console.log(node)
+assert(srcmap.isDynamicArray(node))
--- test/test-truffle.js
+++ test/test-truffle.js
@@ -1,13 +1,13 @@
-"use strict";
+"use strict"
 
 var trufstuf = require("../lib/trufstuf.js")
 var assert = require('assert');
 
-assert(trufstuf.is_truffle_root('.') == false);
+assert(trufstuf.is_truffle_root('.') == false)
 assert(trufstuf.is_truffle_root('./sample-truffle') == true);
 var build_dir = trufstuf.get_build_contracts_dir('./sample-truffle')
 assert(trufstuf.get_build_contracts_dir('./sample-truffle')
-       == './sample-truffle/build/contracts');
+       == './sample-truffle/build/contracts')
 var json_files = trufstuf.get_truffle_build_json_files(build_dir);
 var expected = [ 'integeroverflow.json', 'migrations.json', 'mythril.json'];
 assert(expected.length == json_files.length &&
